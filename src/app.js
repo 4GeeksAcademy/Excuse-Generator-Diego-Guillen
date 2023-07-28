7b106e99fef11b9ac167cd34e930b2a222aeff67
diegoguillen70 <137721556+diegoguillen70@users.noreply.github.com>
2023-07-28 15:51:19 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying",
+];
+
+window.onload = () => {
   //write your code here
+  let index = Math.floor(Math.random() * 3);
   console.log("Hello Rigo from the console!");
+  document.getElementById("exucses").innerHTML = "New Text!";
 };
